84c6fcb2198295ead6efe9b0604874831ad59234
stasisha <stasisha@users.noreply.github.com>
2021-08-11 22:16:52 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+## add required functions
+source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
+source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/error.sh)";
+source /dev/stdin <<< /etc/os-release;
+
+apt-get update && apt-get upgrade 
+
+if [[ "$ID" == "raspbian"* ]]; then
+        # ...
+        
+#TODO chech condition        
+elif [[ "$OSTYPE" == "debian"* ]]; then 
+    #fix broken http://fastmirror.pp.ua
+    #https://www.raspbian.org/RaspbianMirrors
+    local sourcesList="${brewPrefix}/dnsmasq.d"
+    mv "${sourcesList}" "${sourcesList}.original.bk"
+    touch "/etc/apt/sources.list"
+    addLineIfNotExists "deb http://mirror.ox.ac.uk/sites/archive.raspbian.org/archive/raspbian buster main contrib non-free rpi" "${dnsmasqD}/${domainSufix}.conf"
+else
+    echoError "Unsupported OS";
+    return 1
+fi
+
+#Docker
+
+bash -c "$(curl -fsSL https://get.docker.com)"
+
+curl -fsSL  -o get-docker.sh
+
