f1ff937422deaf60f0dc305155fc5c517f57ea3b
stasisha <stasisha@users.noreply.github.com>
2021-08-11 23:36:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,23 @@ source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/b
 
 source /etc/os-release
 
+read -p 'Would you like to install Docker? [y/n]: ' docker
+read -p 'Would you like to install Home Assistant? [y/n]: ' homeAssistant
+read -p 'Would you like to install the latest versions of all the previously installed packages. Reboot required. [y/n]: ' upgradePackages
+
+
+apt-get update
 if [[ "$ID" == "raspbian"* ]]; then
-    #fix broken http://fastmirror.pp.ua
-    #https://www.raspbian.org/RaspbianMirrors
-    local sourcesList="/etc/apt/sources.list"
-    mv "${sourcesList}" "${sourcesList}.original.bk"
-    touch "/etc/apt/sources.list"
-    addLineIfNotExists "deb http://mirror.ox.ac.uk/sites/archive.raspbian.org/archive/raspbian buster main contrib non-free rpi" "${sourcesList}"
-#TODO chech condition debian
+    read -p 'Would you like use University of Oxford Mirror? [y/n]: ' oxfordMirror
+    
+    if [ "$oxfordMirror" == 'y' ] || [ "$oxfordMirror" == 'Y' ]; then
+        #https://www.raspbian.org/RaspbianMirrors
+        local sourcesList="/etc/apt/sources.list"
+        mv "${sourcesList}" "${sourcesList}.original.bk"
+        touch "/etc/apt/sources.list"
+        addLineIfNotExists "deb http://mirror.ox.ac.uk/sites/archive.raspbian.org/archive/raspbian buster main contrib non-free rpi" "${sourcesList}"
+    fi
+#TODO check condition debian
 elif [[ "$OSTYPE" == "debian"* ]]; then
     #placeholder
     echo "debian os";
@@ -21,8 +30,27 @@ else
     return 1
 fi
 
-apt-get update && apt-get upgrade
+if [  "$upgradePackages" == 'y' ] || [ "$upgradePackages" == 'Y' || "$oxfordMirror" == 'y' ] || [ "$oxfordMirror" == 'Y']; then
+    apt-get upgrade
+elif
+    read -p 'Would you like to install the latest versions of update package database. [y/n]: ' upgradePackageDatabase
+fi
 
-#Docker
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/stasisha/home-pi/master/install-docker.sh)"
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/stasisha/home-pi/master/install-home-assistant.sh)"
+
+if [ "$upgradePackageDatabase" == 'y' ] || [ "$upgradePackageDatabase" == 'Y']; then
+    apt-get upgrade
+fi
+
+if [ "$upgradePackages" == 'y' ] || [ "$upgradePackages" == 'Y' ]; then
+    apt-get upgrade
+    sudo reboot
+fi
+
+if [ "$docker" == 'y' ] || [ "$docker" == 'Y'  ]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/stasisha/home-pi/master/install-docker.sh)"
+fi
+
+if [ "$homeAssistant" == 'y' ] || [ "$homeAssistant" == 'Y'  ]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/stasisha/home-pi/master/install-home-assistant.sh)"
+fi
+
