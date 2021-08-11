073a76a808e36fc0f6b6efe14203ca9ce4218e5a
stasisha <stasisha@users.noreply.github.com>
2021-08-12 01:21:38 +0300

    Update install-home-assistant.sh

--- install-home-assistant.sh
+++ install-home-assistant.sh
@@ -3,6 +3,8 @@
 # Install needed Home Assistant Supervised dependencies
 sudo apt-get install network-manager apparmor-utils jq -y
 
+sudo reboot
+
 curl -Lo installer.sh https://raw.githubusercontent.com/home-assistant/supervised-installer/master/installer.sh
 bash installer.sh --machine raspberrypi4
 rm -r installer.sh
