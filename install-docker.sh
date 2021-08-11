64fdb119741dd485c4b58c915ccb1c504e73dbd2
stasisha <stasisha@users.noreply.github.com>
2021-08-12 02:21:10 +0300

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Am I root?
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+
 bash -c "$(curl -fsSL https://get.docker.com)"
 sudo usermod -aG docker $USER
