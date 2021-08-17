fd610efc340176ae23781eb10ce7b321480f9a83
stasisha <stasisha@users.noreply.github.com>
2021-08-18 01:30:19 +0300

    Update install-raspap.sh

--- install-raspap.sh
+++ install-raspap.sh
@@ -10,8 +10,15 @@ sudo apt install netfilter-persistent
 docker run --name raspap -it -d --restart unless-stopped --privileged --network=host -v /sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add SYS_ADMIN jrcichra/raspap-docker
 docker exec -it raspap bash ./setup.sh
 
+# lan
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
 iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+
+# 3g/4g
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE  
+iptables -A FORWARD -i eth1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o eth1 -j ACCEPT
+
 netfilter-persistent save
 docker restart raspap
