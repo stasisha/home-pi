2caf950cdd31e8646f5ef66e7da9cd3f41b8f193
stasisha <stasisha@users.noreply.github.com>
2021-08-18 08:45:56 +0300

    Update install-led-fx.sh

--- install-led-fx.sh
+++ install-led-fx.sh
@@ -6,10 +6,8 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
-
-replace "defaults.ctl.card 0" "defaults.ctl.card 1" "/usr/share/alsa/alsa.conf"
-replace "defaults.pcm.card 0" "defaults.pcm.card 1" "/usr/share/alsa/alsa.conf"
+sed -i  "s/defaults.ctl.card 0/defaults.ctl.card 1/g" "/usr/share/alsa/alsa.conf"
+sed -i  "s/defaults.pcm.card 0/defaults.pcm.card 1/g" "/usr/share/alsa/alsa.conf"
 
 curl -sSL https://install.ledfx.app/ | bash
 
