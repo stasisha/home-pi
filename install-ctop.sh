37f1a37d76acc0d8648def584682be7c7b20a1b0
stasisha <stasisha@users.noreply.github.com>
2021-08-18 11:58:21 +0300

    Update install-ctop.sh

--- install-ctop.sh
+++ install-ctop.sh
@@ -8,5 +8,5 @@ fi
 
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-sudo apt update
-sudo apt install docker-ctop
+apt update
+apt install docker-ctop
