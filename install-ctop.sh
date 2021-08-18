6f294f5509e7f2c9ae17142c2063d1e32a5b7f0f
stasisha <stasisha@users.noreply.github.com>
2021-08-18 12:01:31 +0300

    Update install-ctop.sh

--- install-ctop.sh
+++ install-ctop.sh
@@ -6,7 +6,12 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-apt update
-apt install docker-ctop
+if ! dpkg -s docker-ctop &> /dev/null
+then
+    echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+    wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+    apt update
+    apt install docker-ctop
+else
+    echo "docker-ctop is already installed. Nothing to do."
+fi
