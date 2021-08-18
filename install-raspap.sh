bc6db29d898751f3e86ab9b6714ef732c4b3f557
stasisha <stasisha@users.noreply.github.com>
2021-08-18 11:49:52 +0300

    Update install-raspap.sh

--- install-raspap.sh
+++ install-raspap.sh
@@ -6,19 +6,26 @@ if [ "x$(id -u)" != 'x0' ]; then
     exit 1
 fi
 
+read -p 'Would you like to install RaspAP (docker)? [y/n]: ' raspapDocker
+read -p 'Would you like to configure network? [y/n]: ' configureNetwork
+
 source /dev/stdin <<< "$(curl -fsSL https://raw.githubusercontent.com/stasisha/bash-utils/master/file-edit.sh)";
 
-apt install netfilter-persistent
-docker run --name raspap -it -d --restart unless-stopped --privileged --network=host -v /sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add SYS_ADMIN jrcichra/raspap-docker
-docker exec -it raspap bash ./setup.sh
 
-# allow internet access 
-curl -K "https://raw.githubusercontent.com/stasisha/home-pi/main/startup-rasp-ap.sh" -o "/etc/init.d/startup-rasp-ap.sh"
-chmod +x /etc/init.d/startup-rasp-ap.sh
-ln -s /etc/init.d/startup-rasp-ap.sh /etc/rc3.d/startup-rasp-ap
-sh ./etc/init.d/startup-rasp-ap.sh
+if [ "$raspapDocker" == 'y' ] || [ "$raspapDocker" == 'Y'  ]; then
+    docker run --name raspap -it -d --restart unless-stopped --privileged --network=host -v /sys/fs/cgroup:/sys/fs/cgroup:ro --cap-add SYS_ADMIN jrcichra/raspap-docker
+    docker exec -it raspap bash ./setup.sh
+    docker restart raspap
+fi
 
-# routing traffic between networks
-addLineToBottomIfNotExists "net.ipv4.conf.all.forwarding=1" "/etc/sysctl.conf"
+if [ "$configureNetwork" == 'y' ] || [ "$configureNetwork" == 'Y'  ]; then
+    apt install netfilter-persistent -y
+    # allow internet access 
+    curl -K "https://raw.githubusercontent.com/stasisha/home-pi/main/startup-rasp-ap.sh" -o "/etc/init.d/startup-rasp-ap.sh"
+    chmod +x /etc/init.d/startup-rasp-ap.sh
+    ln -s /etc/init.d/startup-rasp-ap.sh /etc/rc3.d/startup-rasp-ap
+    sh ./etc/init.d/startup-rasp-ap.sh
 
-docker restart raspap
+    # routing traffic between networks
+    addLineToBottomIfNotExists "net.ipv4.conf.all.forwarding=1" "/etc/sysctl.conf"
+fi
